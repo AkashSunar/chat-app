13af8cb4a1a3e55017b5d590ce4700c916087c6c
AkashSunar <sunarakash1122@gmail.com>
2026-01-21 12:20:39 +0545

    fix/ otp page code refactored

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
 import { clsx, type ClassValue } from "clsx";
+import { randomBytes } from "crypto";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+export function getAuthToken() {
+  if (typeof window === "undefined") return null;
+  const match = document.cookie.match(/sb-[^=]+-auth-token=([^;]+)/);
+  return match ? match[1] : null;
+}
+
+export function generateRandomPassword(length: number = 16): string {
+  return randomBytes(length).toString("hex");
 }
