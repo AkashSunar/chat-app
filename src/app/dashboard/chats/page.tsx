e002e9c72546b6b068e3cc1aad49007bc7f0e1a0
AkashSunar <sunarakash1122@gmail.com>
2026-01-30 22:56:14 +0545

    chat history page added

---
+++ src/app/dashboard/chats/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ChatHistory() {
+  return (
+    <div className="flex items-center justify-center h-screen font-bold">
+      Welcome to Chat History Page
+    </div>
+  );
+}
+
+export default ChatHistory;
