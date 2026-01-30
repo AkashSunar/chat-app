0dda67fcdf6b506f769a2c929f40316b95127b39
AkashSunar <sunarakash1122@gmail.com>
2026-01-30 23:17:32 +0545

    settings page added

---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className="flex items-center justify-center h-screen font-bold">
+      Welcome to Settings Page
+    </div>
+  );
+}
+
+export default page;
