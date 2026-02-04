52c0ea2e93cb666ae691acab1aca7f899e89532f
AkashSunar <sunarakash1122@gmail.com>
2026-02-04 23:57:45 +0545

    asistant page added

---
+++ src/app/dashboard/assistant/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BotAssistant() {
+  return (
+    <div className="flex items-center justify-center h-screen font-bold">
+      Welcome to Chat History Page
+    </div>
+  );
+}
+
+export default BotAssistant;
