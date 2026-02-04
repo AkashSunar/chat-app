03d3aedc7870f11eb996616f143cacae394f83f3
AkashSunar <sunarakash1122@gmail.com>
2026-02-04 23:58:30 +0545

    asistant page added

--- src/app/dashboard/assistant/page.tsx
+++ src/app/dashboard/assistant/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function BotAssistant() {
   return (
     <div className="flex items-center justify-center h-screen font-bold">
-      Welcome to asistant Page
+      Welcome to asistant Page,wher you can talk with bot
     </div>
   );
 }
