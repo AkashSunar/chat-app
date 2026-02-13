6ad07ed886d85633744a5c601e89b5dbf4026143
AkashSunar <sunarakash1122@gmail.com>
2026-02-13 23:35:01 +0545

    console log removed

--- src/sections/chats/chat.tsx
+++ src/sections/chats/chat.tsx
@@ -28,9 +28,7 @@ type Message = {
 export default function Chat() {
   const [messages, setMessages] = useState<Message[]>([]);
   const { user } = useAuth();
-  if (user) {
-    console.log("user:", user);
-  }
+
   const getInitials = (name: string) => {
     const names = name.split(" ");
     const initials = names.map((n) => n[0]).join("");
