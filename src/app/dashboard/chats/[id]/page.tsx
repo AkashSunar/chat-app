dec6e8d99283ea5d8e7960b6a2c8c5c5546e6d8f
AkashSunar <sunarakash1122@gmail.com>
2026-02-14 23:51:49 +0545

    page added inside chat folder

---
+++ src/app/dashboard/chats/[id]/page.tsx
@@ -0,0 +1,12 @@
+import Chat from "@/sections/chats/chat";
+import React from "react";
+
+function page() {
+  return (
+    <>
+      <div className="flex flex-col h-screen">Welcome to the chat page!</div>
+    </>
+  );
+}
+
+export default page;
